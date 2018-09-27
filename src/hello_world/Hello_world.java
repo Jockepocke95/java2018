dbff55cee841ad212a1c9af324a73d5460d09bc4
Joakim Andersson <Joakim Andersson@DESKTOP-JR0J0MQ>
2018-09-27 10:19:03 +0200

    This is me Commiting.

---
+++ src/hello_world/Hello_world.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author Joakim Andersson
+ */
+public class Hello_world {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ctr + space is awesome!");
+        
+        double decimalNum = 1.22;
+        int decInInt = (int) decimalNum;
+        System.out.println(decInInt);
+        System.out.println("alt+shift+F to format");
+        
+    }
+    
+}
